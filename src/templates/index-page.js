6d39849a9edb0eeccafb881fe3badf2a9db4134d
Nick Gamb <nickgamb@gmail.com>
2024-07-08 22:57:08 -0700

    Update index-page.js

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -48,8 +48,8 @@ export const IndexPageTemplate = ({
                   <Features gridItems={intro.blurbs} />
                   <div className="columns">
                     <div className="column is-12 has-text-centered">
-                      <Link className="btn" to="/products">
-                        See all products
+                      <Link className="btn" to="/about">
+                        About MindGarden AI
                       </Link>
                     </div>
                   </div>
