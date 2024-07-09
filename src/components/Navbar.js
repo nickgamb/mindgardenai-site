1eb0239535e6fde73b19755d637cf110e3131ad7
Nick Gamb <nickgamb@gmail.com>
2024-07-08 23:31:44 -0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -61,6 +61,18 @@ const Navbar = () => {
               Contact
             </Link>
           </li>
+          <li className="navbar-end has-text-centered">
+            <a
+              className="navbar-item"
+              href="https://github.com/nickgamb/mindgardenai-site"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <span className="icon">
+                <img src={github} alt="Github" />
+              </span>
+            </a>
+          </li>
         </ul>
       </div>
     </nav>
