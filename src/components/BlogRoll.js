1d41599f35c16b2fb5b737d2d56bbc98ec979e7d
Nick Gamb <nickgamb@gmail.com>
2024-07-22 15:44:35 -0700

    Update BlogRoll.js

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -52,7 +52,7 @@ const BlogRollTemplate = (props) => {
                 {post.excerpt}
                 <br />
                 <br />
-                <Link className="button" to={post.fields.slug}>
+                <Link className="btn" to={post.fields.slug}>
                   Keep Reading →
                 </Link>
               </p>
