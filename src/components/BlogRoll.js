c3faa1dbd460897d0f3996bbb5a7b84b1cb685c1
Nick Gamb <nickgamb@gmail.com>
2024-07-21 20:27:31 -0700

    Update BlogRoll.js

--- src/components/BlogRoll.js
+++ src/components/BlogRoll.js
@@ -14,7 +14,7 @@ const BlogRollTemplate = (props) => {
         posts.map(({ node: post }) => (
           <div className="is-parent column is-6" key={post.id}>
             <article
-              className={`blog-list-item tile is-child box notification ${
+              className={`blog-list-item tile is-child box ${
                 post.frontmatter.featuredpost ? 'is-featured' : ''
               }`}
             >
