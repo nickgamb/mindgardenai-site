84d222ace9020426c480b13bfa97d11a683ae0b4
Nick Gamb <nickgamb@gmail.com>
2024-07-22 20:25:50 -0700

    Updates

--- src/templates/podcast-page.js
+++ src/templates/podcast-page.js
@@ -14,7 +14,7 @@ export const PodcastPageTemplate = ({ title, content, contentComponent }) => {
         <div className="columns">
           <div className="column is-10 is-offset-1">
             <div className="section">
-              <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
+              <h2 className="title is-size-3 has-text-weight-bold is-bold-light full-width-content">
                 {title}
               </h2>
               <PageContent className="content" content={content} />
