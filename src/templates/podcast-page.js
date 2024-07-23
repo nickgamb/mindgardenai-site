278277881f528b827f79639b8c3a61c8e68ed552
Nick Gamb <nickgamb@gmail.com>
2024-07-22 17:59:18 -0700

    Update podcast-page.js

--- src/templates/podcast-page.js
+++ src/templates/podcast-page.js
@@ -14,7 +14,7 @@ export const PodcastPageTemplate = ({ title, content, contentComponent }) => {
           <div className="content">
             <div className="feature-section">
               <h3 className="has-text-weight-semibold is-size-2 section-title">{title}</h3>
-              <PageContent className="section-description-blk" content={content} />
+              <p className="section-description-blk" content={content} />
             </div>
           </div>
         </div>
