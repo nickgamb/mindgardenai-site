c4e5e93c2a600da346cbe61d3c36c2a7bf3487f7
Nick Gamb <nickgamb@gmail.com>
2024-07-23 12:05:14 -0700

    Update index.js

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -88,7 +88,7 @@ export default class Index extends React.Component {
                 action="/contact/thanks/"
                 data-netlify="true"
                 data-netlify-honeypot="bot-field"
-                onSubmit={this.handleSubmit}
+                //onSubmit={this.handleSubmit}
               >
                 {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
                 <input type="hidden" name="form-name" value="contact" />
