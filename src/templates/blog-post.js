f1dba28bebd14d0d0240d71ae49e38dcc8cd8472
Nick Gamb <nickgamb@gmail.com>
2024-09-28 14:58:04 -0700

    Updating code block styles
    Updating code block styles

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -6,6 +6,25 @@ import { graphql, Link } from "gatsby";
 import Layout from "../components/Layout";
 import Content, { HTMLContent } from "../components/Content";
 
+
+document.addEventListener('DOMContentLoaded', function() {
+  var copyButtons = document.querySelectorAll('.copy-button');
+  copyButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+      var codeBlock = this.previousElementSibling;
+      if (codeBlock) {
+        var text = codeBlock.innerText;
+        navigator.clipboard.writeText(text).then(function() {
+          button.textContent = 'Copied!';
+          setTimeout(function() {
+            button.textContent = 'Copy';
+          }, 2000);
+        });
+      }
+    });
+  });
+});
+
 // eslint-disable-next-line
 export const BlogPostTemplate = ({
   content,
