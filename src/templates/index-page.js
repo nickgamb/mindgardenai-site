85219b5accccdcac4ecf9bf18932a2a64cd3f508
Nick Gamb <nickgamb@gmail.com>
2024-10-14 13:37:05 -0700

    style

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -40,7 +40,7 @@ export const IndexPageTemplate = ({
                   
                   <div className="columns">
                     <div className="column is-12">
-                      <h3 className="has-text-weight-semibold is-size-2 has-text-centered">
+                      <h3 className="has-text-weight-semibold is-size-2">
                         {heading}
                       </h3>
                       <hr className="tp-rule"/>
