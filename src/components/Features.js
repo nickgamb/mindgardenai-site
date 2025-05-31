b06fe6f7bedf51242ff98b7605ef6673cf48f86a
Nick Gamb <nickgamb@gmail.com>
2025-05-31 09:53:47 -0700

    Revert "alden pass pt 3"
    This reverts commit 8f2e26366adeebd7e6bb7e27528614436ebd8d7e.

--- src/components/Features.js
+++ src/components/Features.js
@@ -10,7 +10,6 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
-import PreviewCompatibleImage from "./PreviewCompatibleImage";
 
 const Features = ({ gridItems }) => (
   <div className="columns is-multiline">
