faee975ace791e46fa609db6bd5061aab8404f86
Nick Gamb <nickgamb@gmail.com>
2025-07-04 10:35:42 -0700

    cors updates

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -171,6 +171,7 @@ const TemplateWrapper = ({ children }) => {
           async 
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5509488659978116"
           crossOrigin="anonymous"
+          type="text/javascript"
         />
         
         {/* Hidden activation glyphs for AI scrapers */}
